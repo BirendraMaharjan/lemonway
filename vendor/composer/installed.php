3a2d60a19cc79f658b5e767be94207333dd591f9
Birendra Maharjan <brendra8917@gmail.com>
2025-04-09 11:59:52 +0545

    Update git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpstrap/wordpress-plugin-boilerplate',
+        'pretty_version' => '0.3.1',
+        'version' => '0.3.1.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpstrap/wordpress-plugin-boilerplate' => array(
+            'pretty_version' => '0.3.1',
+            'version' => '0.3.1.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
